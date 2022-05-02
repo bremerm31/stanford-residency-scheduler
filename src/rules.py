5778bc7cb0de4922404694883cade231d8c7acbe
Max Bremer <bremerm31@gmail.com>
2022-05-01 21:18:00 -0700

    Adding more rules
    Adding `inBlocks` and `sequence` rule. Fixing issue in
    `contiguousBlock` wherein after a single block single sequences could
    still exist.

--- src/rules.py
+++ src/rules.py
@@ -42,7 +42,6 @@ class Rule:
 
             return [resident_idx]
 
-
 class doBefore(Rule):
     def __init__(self,week_id,service_name, who):
         super().__init__("do_before")
@@ -81,49 +80,20 @@ class doAfter(Rule):
                       == 0 for r in r_indices),
                      self.name)
 
-'''
 class inBlocks(Rule):
-    def __init__(self,name, block_size, service_name):
-        super(Rule, self).__init__(name)
+    def __init__(self, block_size, service_name, who):
+        super().__init__("in"+str(block_size)+"WeekBlocks_"+service_name)
         self.block_size = block_size
         self.service_name = service_name
-    def addRuleToModel(self, model, schedule, residents, services):
-        service_idx = -1
-        for i, s in enumerate(services):
-            if s.name == self.service_name:
-                service_idx = i
-                break
-
-        if service_idx == -1:
-            raise RuleException("Service: "+self.service_name+
-                                " not found in list of services")
-
-        #need to add rule here
-'''
-class upperBound(Rule):
-    def __init__(self, service_name, count):
-        super().__init__("upperBound")
-        self.service_name = service_name
-        self.count   = int(count)
+        self.who = who
     def addRuleToModel(self, scheduler, residents, services):
         s_idx = super().getServiceIndex(self.service_name, services)
+        r_indices = super().getResidentIndices(self.who, residents)
 
-        scheduler.model.addConstrs(
-            (scheduler.schedule.sum(r,s_idx,'*') <= self.count
-             for r in range(len(residents))),
-             name=self.name)
+        s = scheduler.schedule
+        m = scheduler.model
 
-class singleBlock(Rule):
-    def __init__(self, service_name):
-        super().__init__(service_name+"singleBlock")
-        self.service_name = service_name
-    def addRuleToModel(self, scheduler, residents, services):
-        s_idx = super().getServiceIndex(self.service_name, services)
-
-        m  = scheduler.model
-        s  = scheduler.schedule
-
-        for r_idx in range(len(residents)):
+        for r_idx in r_indices:
             r = residents[r_idx]
             service_lb = r.service_lbs[self.service_name]
             
@@ -131,19 +101,95 @@ class singleBlock(Rule):
                 continue
             
             start = m.addVars(
-                schedulingModel.n_weeks-service_lb,
+                schedulingModel.n_weeks-self.block_size,
                 vtype = GRB.BINARY,
                 name = r.name+'_'+self.service_name+'_start')
 
+            n_starts = service_lb // self.block_size
+            assert n_starts * self.block_size == service_lb
 
-            m.addConstr(start.sum('*') == 1, name="start_once_"+self.name+"_"+r.name)
+            m.addConstr(start.sum('*') == n_starts, name="n_starts_"+self.name+"_"+r.name)
 
-            m.addConstrs((gb.quicksum(s[r_idx,s_idx,tt] for tt in range(t,t+service_lb))
-                          >= service_lb * start[t] for t in range(schedulingModel.n_weeks-service_lb)),
+            # require contiguous blocks of block_size 
+            m.addConstrs((gb.quicksum(s[r_idx,s_idx,tt] for tt in range(t,t+self.block_size))
+                          >= self.block_size * start[t] for t in range(schedulingModel.n_weeks-self.block_size)),
                          name=self.name)
-            
+            #starts can't double count an interval (i.e. one start per "block_size")
+            m.addConstrs((gb.quicksum(start[tt] for tt in range(t,t+self.block_size))
+                          <= 1 for t in range(schedulingModel.n_weeks-2*self.block_size)),
+                         name=self.name+"_no_block_overlap")
 
+class upperBound(Rule):
+    def __init__(self, service_name, count):
+        super().__init__("upperBound")
+        self.service_name = service_name
+        self.count   = count
+    def addRuleToModel(self, scheduler, residents, services):
+        s_idx = super().getServiceIndex(self.service_name, services)
+
+        if self.count:
+            scheduler.model.addConstrs(
+                (scheduler.schedule.sum(r,s_idx,'*') <= self.count
+                 for r in range(len(residents))),
+                name=self.name)
+        else:
+            scheduler.model.addConstrs(
+                (scheduler.schedule.sum(r,s_idx,'*')
+                 <= residents[r].service_lbs[self.service_name]
+                 for r in range(len(residents))),
+                name=self.name)
+
+class singleBlock(Rule):
+    def __init__(self, service_name, who):
+        super().__init__(service_name+"singleBlock")
+        self.service_name = service_name
+        self.who = who
+    def addRuleToModel(self, scheduler, residents, services):
+        s_idx = super().getServiceIndex(self.service_name, services)
+        r_indices = super().getResidentIndices(self.who, residents)
+
+        m  = scheduler.model
+        s  = scheduler.schedule
+
+        for r_idx in r_indices:
+            r = residents[r_idx]
+            service_lb = r.service_lbs[self.service_name]
             
+            if service_lb == 0:
+                continue
+            
+            start = m.addVar(
+                vtype = GRB.INTEGER,
+                lb=0, ub=schedulingModel.n_weeks-service_lb,
+                name = r.name+'_'+self.service_name+'_start')
+
+            stop = m.addVar(
+                vtype=GRB.INTEGER,
+                lb=service_lb, ub=schedulingModel.n_weeks,
+                name = r.name+'_'+self.service_name+'_stop')
+
+            m.addConstr(stop - start == s.sum(r_idx, s_idx,'*'),
+                         name=self.name)
+
+class sequence(Rule):
+    def __init__(self, first_service, second_service, who):
+        super().__init__("sequence")
+        self.first_service = first_service
+        self.second_service = second_service
+        self.who = who
+    def addRuleToModel(self, scheduler, residents, services):
+        s_first_idx = super().getServiceIndex(self.first_service, services)
+        s_second_idx = super().getServiceIndex(self.second_service, services)
+        r_indices = super().getResidentIndices(self.who, residents)
+
+        m  = scheduler.model
+        s  = scheduler.schedule
+
+        m.addConstrs((s[r_idx,s_first_idx,t] == s[r_idx,s_second_idx,t+1]
+                      for r_idx in r_indices
+                      for t in range(schedulingModel.n_weeks-1)),
+                      name=self.name)
+        
 
 def RuleFactory(rule_type):
     assert len(rule_type.keys())==1
@@ -168,14 +214,22 @@ def RuleFactory(rule_type):
     elif name == "in_blocks":
         assert "block_size" in arg_dict
         assert "service" in arg_dict
-        return inBlocks(arg_dict["name"],
-                        arg_dict["block_size"],
-                        arg_dict["service"])
+        return inBlocks(arg_dict["block_size"],
+                        arg_dict["service"],
+                        arg_dict["who"])
     elif name == "upper_bound":
         assert "service" in arg_dict
-        assert "count"   in arg_dict
+        if "count" not in arg_dict:
+            arg_dict["count"] = None
         return upperBound(arg_dict["service"],
                           arg_dict["count"])
     elif name == "single_block":
         assert "service" in arg_dict
-        return singleBlock(arg_dict["service"])           
+        return singleBlock(arg_dict["service"],
+                           arg_dict["who"])
+    elif name == "sequence":
+        assert "first" in arg_dict
+        assert "second" in arg_dict
+        return sequence(arg_dict["first"],
+                        arg_dict["second"],
+                        arg_dict["who"])
